9a0e891d76d3271a7f10c0adeb835521837dc64d
Gatti Programador Junior <167263639+EduardGatti@users.noreply.github.com>
2024-12-04 21:22:05 -0300

    Add files via upload

--- src/comum/componentes/CardProduto/CardProduto.jsx
+++ src/comum/componentes/CardProduto/CardProduto.jsx
@@ -1,17 +1,21 @@
 // src/comum/componentes/CardProduto/CardProduto.jsx
 import React from 'react';
 import './CardProduto.css';  // Aqui você pode estilizar seu card.
+import { Link } from 'react-router-dom';
 
 const CardProduto = ({ produto }) => {
-  return (
-    <div className="card-produto">
-      <h3>{produto.nome_produto}</h3>
-      <p>Preço: R${produto.preco_produto}</p>
-      <p>{produto.descricao_produto}</p>
-      <img src='{produto.foto_produto}'></img>
-      {/* <Link to=""></Link> */}
-    </div>
-  );
+    return (
+        <Link to="/nada">
+            <div className="card-produto">
+                <img src={produto.foto_produto}></img>
+                <div>
+                    <h3>{produto.nome_produto}</h3>
+                    <p>Preço: R${produto.preco_produto}</p>
+                    <p>{produto.descricao_produto.substr(0, 100) + "..."}</p>
+                </div>
+            </div>
+        </Link>
+    );
 };
 
 export default CardProduto;
