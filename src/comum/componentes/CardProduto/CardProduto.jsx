2e3e9b83d542db60824b089352ebb7ce445788c2
Gatti Programador Junior <eduardogatti600@gmail.com>
2024-12-11 18:49:49 -0300

    att novo

--- src/comum/componentes/CardProduto/CardProduto.jsx
+++ src/comum/componentes/CardProduto/CardProduto.jsx
@@ -9,7 +9,7 @@ const CardProduto = ({ produto }) => {
             <div className="card-produto">
                 <img src={produto.foto_produto}></img>
                 <div>
-                    <h4>{produto.nome_produto}</h4>
+                    <h4>{produto.nome_produto.substr(0, 20)}</h4>
                     <p>Preço: R${produto.preco_produto}</p>
                     <p>{produto.descricao_produto.substr(0, 20) + "..."}</p>
                     <button >Ver Detalhes...</button>
